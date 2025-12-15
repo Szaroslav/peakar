b4931818efdaee7924900a08c79a29c5351bdba3
szymo <szymon.glomski@gmail.com>
2025-12-15 18:18:32 +0100

    Place nearby peaks logic in separate hook

--- app/(tabs)/elevationMap.tsx
+++ app/(tabs)/elevationMap.tsx
@@ -1,5 +1,3 @@
-import * as Location from "expo-location";
-import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   Dimensions,
@@ -7,66 +5,13 @@ import {
   Text,
   View,
 } from "react-native";
-
-import { MapPoint, RenderablePeak } from "@/models/map";
-import { getElevation } from "@/services/elevationApi";
-import { getPeaksInArea } from "@/services/openStreetMapApi";
-import { transformToRenderablePeaks } from "@/utils/transformToRenderablePeaks";
+import { useNearbyPeaks } from "@/hooks/use-nearby-peaks";
 
 const { width, height } = Dimensions.get("window");
-const OBSERVER_HEIGHT = 1.6;
-const LINE_SEGMENT_LENGTH = 250;
-const NEARBY_PEAKS_RADIUS = 25000;
 const POINT_SIZE = 12;
 
 export default function ElevationMap() {
-  const [loading, setLoading] = useState(true);
-  const [peaks, setPeaks] = useState<RenderablePeak[]>([]);
-  const [currentLocation, setCurrentLocation] = useState<MapPoint | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-          setError("Location permission denied");
-          setLoading(false);
-          return;
-        }
-
-        // Get current location
-        const loc = await Location.getCurrentPositionAsync({});
-        const current: MapPoint = {
-          latitude: loc.coords.latitude,
-          longitude: loc.coords.longitude,
-          elevation: await getElevation(
-            loc.coords.latitude,
-            loc.coords.longitude,
-          ),
-        };
-        setCurrentLocation(current);
-        console.log("Current location with elevation:", current);
-
-        const nearbyPeaks = await getPeaksInArea(current, NEARBY_PEAKS_RADIUS);
-        console.log("Nearby peaks:", nearbyPeaks);
-
-        const renderablePeaks = await transformToRenderablePeaks(
-          current,
-          nearbyPeaks,
-          {
-            observerHeight: OBSERVER_HEIGHT,
-            lineSegmentLength: LINE_SEGMENT_LENGTH,
-          },
-        );
-        setPeaks(renderablePeaks);
-      } catch (err: any) {
-        setError(err.message || "Unexpected error");
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, []);
+  const { peaks, currentLocation, loading, error } = useNearbyPeaks();
 
   if (loading) {
     return (
