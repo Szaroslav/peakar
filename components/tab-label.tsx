f9f2ba0b89e65512af27d1a6755408d5ca3232fb
Jakub Szaredko <jakub.szaredko@gmail.com>
2026-01-11 19:29:46 +0100

    Feat: Emulate landscape screen orientation in camera tab (#20)

---
+++ components/tab-label.tsx
@@ -0,0 +1,42 @@
+import { SymbolViewProps } from "expo-symbols";
+import { useEffect, useRef } from "react";
+import { Animated, Easing, Text } from "react-native";
+
+import { IconSymbol } from "./ui/icon-symbol";
+
+interface TabLabelProps {
+  name: string;
+  color: string;
+  rotated: boolean;
+  icon: SymbolViewProps["name"];
+}
+
+export function CustomTabLabel({ name, color, rotated, icon }: TabLabelProps) {
+  const rotation = useRef(new Animated.Value(rotated ? 1 : 0)).current;
+
+  useEffect(() => {
+    Animated.timing(rotation, {
+      toValue: rotated ? 1 : 0,
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+      useNativeDriver: true,
+    }).start();
+  }, [rotated]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const rotate = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "90deg"],
+  });
+
+  return (
+    <Animated.View
+      style={{
+        transform: [{ rotate }],
+        alignItems: "center",
+      }}
+    >
+      <IconSymbol name={icon} size={28} color={color} />
+      <Text style={{ color, fontSize: 12 }}>{name}</Text>
+    </Animated.View>
+  );
+}
